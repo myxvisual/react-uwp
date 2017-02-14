4762bc5d6f9cdabd3f8a1dc1c2cc09fd455280c0
myxvisual <myxvisual@live.com>
2017-02-14 18:30:58 +0800

    Remove TestIcons Data

--- components/Icon/gen.js
+++
@@ -1,8 +0,0 @@
-const fs = require('fs')
-let data = fs.readFileSync('./icons.txt', 'utf8')
-data = data.split('\n')
-data = data.map(str => ({
-	[str.slice(5).replace('\r', '')]: `\\u${str.slice(0, 4)}`
-})).reduce((prev, curr) => Object.assign({}, prev, curr), {})
-console.log(data)
-fs.writeFileSync('icons.ts', `export default ${JSON.stringify(data)}`)
