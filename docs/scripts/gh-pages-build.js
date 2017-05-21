e7cc84e12d1dbef67493a450f702818bad6dba63
myxvisual <myxvisual@live.com>
2017-05-21 23:28:52 +0800

    finished gh-pages-build script

--- docs/scripts/gh-pages-build.js
+++ docs/scripts/gh-pages-build.js
@@ -5,8 +5,10 @@ const usage = '\nbuild <vn.n.n[-pre[.n]]> | <HEAD> [-p]\n'
 const versionsFile = './src/assets/versions.json'
 
 const args = process.argv
-
-if (args.length < 3) exit(usage)
+if (args.length < 3) {
+  console.log(usage, '\n')
+  process.exit()
+}
 
 const version = args[2]
 const useForcePush = args[3] === '-p'
@@ -24,4 +26,26 @@ function execSyncWithLog(command) {
 function buildDocs() {
   process.chdir(__dirname)
   execSyncWithLog('git checkout gh-pages')
+  execSyncWithLog('git reset --hard HEAD~1')
+
+  if (version === 'HEAD') {
+    execSyncWithLog('git checkout --detach master')
+  } else {
+    execSyncWithLog(`git checkout tags/${version}`)
+  }
+
+  execSyncWithLog('test -d \"./build\" && rm -r \"./build\" || exit 0')
+  execSyncWithLog('cd ../ && npm install && cd docs && npm run build')
+  
+  execSyncWithLog('git checkout gh-pages')
+
+  if (version === 'HEAD') {
+    execSyncWithLog('git commit --amend --no-edit')
+  } else {
+    execSyncWithLog(`git add .. && git commit -m '${version}'`)
+  }
+
+  if (useForcePush) {
+    execSyncWithLog('git push -f')
+  }
 }
