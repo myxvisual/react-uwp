14f877ce81f3cfeaefbd473cd159fb4cc1331970
myxvisual <myxvisual@live.com>
2017-05-22 13:19:43 +0800

    update build script

--- docs/scripts/gh-pages-build.js
+++ docs/scripts/gh-pages-build.js
@@ -1,4 +1,5 @@
 const fs = require('fs')
+const path = require('path')
 const { execSync } = require('child_process')
 
 const usage = '\nbuild <vn.n.n[-pre[.n]]> | <HEAD> [-p]\n'
@@ -15,7 +16,7 @@ let versions
 const versionIsHEAD = version === 'HEAD'
 const useForcePush = args[3] === '-p'
 const versionNumber = versionIsHEAD ? (
-  JSON.parse(fs.writeFileSync('../../package.json', 'utf8')).version
+  JSON.parse(fs.writeFileSync(path.resolve(__dirname, '../../package.json'), 'utf8')).version
 ) : version
 
 function execSyncWithLog(command) {
