927f783b0b47a91b2d48ad4ec4749de18aa8599f
myxvisual <myxvisual@live.com>
2017-05-24 13:49:46 +0800

    Add ScrollReveal Component

---
+++ src/ScrollReveal/index.tsx
@@ -0,0 +1,32 @@
+import * as React from "react";
+import * as PropTypes from "prop-types";
+
+import ThemeType from "../styles/ThemeType";
+
+export interface DataProps {}
+
+export interface ScrollRevealProps extends DataProps, React.HTMLAttributes<HTMLDivElement> {}
+
+export interface ScrollRevealState {}
+
+export default class ScrollReveal extends React.Component<ScrollRevealProps, ScrollRevealState> {
+  static defaultProps: ScrollRevealProps = {};
+
+  state: ScrollRevealState = {};
+
+  static contextTypes = { theme: PropTypes.object };
+  context: { theme: ThemeType };
+
+  render() {
+    const { ...attributes } = this.props;
+    const { theme } = this.context;
+
+    return (
+      <div
+        {...attributes}
+      >
+        ScrollReveal
+      </div>
+    );
+  }
+}
