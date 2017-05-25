0bef5114ac8bccfb4daaac0c5b07b505176b333a
myxvisual <myxvisual@live.com>
2017-05-26 03:36:41 +0800

    Update StrupedBackground function

--- src/styles/StripedBackground.ts
+++ src/styles/StripedBackground.ts
@@ -1,4 +1,4 @@
-export default function getStripedBackground(size = 4, firstColor = "#000", secondColor = "hsla(0, 0%, 0%, 0)") {
+export default function getStripedBackground(size = 4, firstColor = "#000", secondColor = "hsla(0, 0%, 0%, 0.85)") {
   return {
     background: `linear-gradient(45deg, ${firstColor} 25%, ${secondColor} 0, ${secondColor} 50%, ${firstColor} 0, ${firstColor} 75%, ${secondColor} 0)`,
     backgroundSize: `${size}px ${size}px`
