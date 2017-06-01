b38ec54661069c8b69424bb213fa121857fb505f
myxvisual <myxvisual@live.com>
2017-06-01 18:32:45 +0800

    Update Flyout Docs

--- docs/src/routes/Components/Flyout/SimpleExample.tsx
+++ docs/src/routes/Components/Flyout/SimpleExample.tsx
@@ -2,6 +2,7 @@ import * as React from "react";
 import * as PropTypes from "prop-types";
 
 import Flyout from "react-uwp/Flyout";
+import Button from "react-uwp/Button";
 import FlyoutContent from "react-uwp/FlyoutContent";
 
 export default class SimpleExample extends React.Component<{}, void> {
@@ -11,18 +12,18 @@ export default class SimpleExample extends React.Component<{}, void> {
   render() {
     return (
       <Flyout>
-        <p>Your Content Component</p>
+        <Button>Your Content Component</Button>
 
         <FlyoutContent
           show={false}
-          style={{ height: 80 }}
+          style={{ height: 40 }}
         >
           Your Top FlyoutContent
         </FlyoutContent>
         <FlyoutContent
           show={false}
           verticalPosition="bottom"
-          style={{ height: 80 }}
+          style={{ height: 60 }}
           enterDelay={850}
         >
           Your Bottom FlyoutContent with delay 850ms
