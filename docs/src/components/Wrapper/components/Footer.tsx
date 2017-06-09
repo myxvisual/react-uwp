fad41d5a2eb62eb14d51d18267ada6b475a8c890
myxvisual <myxvisual@live.com>
2017-06-09 13:15:53 +0800

    Update Footer Theme

--- docs/src/components/Wrapper/components/Footer.tsx
+++ docs/src/components/Wrapper/components/Footer.tsx
@@ -91,7 +91,7 @@ function getStyles(footer: Footer): {
     root: prepareStyles({
       fontSize: 14,
       color: theme.baseHigh,
-      background: theme.isDarkTheme ? "hsla(0, 0%, 6%, 0.85)" : "hsla(0, 0%, 94%, 0.85)",
+      background: theme.isDarkTheme ? "hsla(0, 0%, 0%, 0.95)" : "hsla(0, 0%, 100%, 0.95)",
       boxShadow: theme.isDarkTheme ? void 0 : `0 -2px 8px ${theme.listLow}`,
       minHeight: footerHeight,
       ...style
