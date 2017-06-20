589b1e4e1cd7a274be38589b1806aa88e93e1441
myxvisual <myxvisual@live.com>
2017-06-20 15:26:16 +0800

    fix: Fixed package.json devDependencies modules

--- scripts/rewriteFile.js
+++ scripts/rewriteFile.js
@@ -7,30 +7,7 @@ const packageFile = '../build/package.json'
 const typingFile = '../build/index.d.ts'
 const packageData = JSON.parse(fs.readFileSync(packageFile))
 packageData.scripts = void 0
-const unUseDevDependencies = [
-  'babel-eslint',
-  'babel-eslint',
-  'babel-preset-es2015',
-  'babel-preset-react',
-  'babel-preset-stage-0',
-  'conventional-changelog-cli',
-  'cross-env',
-  'eslint',
-  'eslint-config-airbnb',
-  'eslint-plugin-babel',
-  'eslint-plugin-import',
-  'eslint-plugin-jsx-a11y',
-  'eslint-plugin-react',
-  'fs-extra',
-  'husky',
-  'npm-run-all',
-  'tslint',
-  'tslint-eslint-rules',
-  'tslint-no-circular-imports'
-]
-for (const devDependencies of unUseDevDependencies) {
-  packageData.devDependencies[devDependencies] = void 0
-}
+packageData.devDependencies = void 0
 
 fs.writeFileSync(packageFile, JSON.stringify(packageData, null, 2))
 
