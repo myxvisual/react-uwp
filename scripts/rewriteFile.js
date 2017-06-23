483cd95e36df4a2973323c8a90be37339e8d7dbd
myxvisual <myxvisual@live.com>
2017-06-23 09:53:42 +0800

    chore: Add new @types/node version and remove public @types/react-router

--- scripts/rewriteFile.js
+++ scripts/rewriteFile.js
@@ -7,6 +7,7 @@ const packageFile = '../build/package.json'
 const typingFile = '../build/index.d.ts'
 const packageData = JSON.parse(fs.readFileSync(packageFile))
 packageData.scripts = void 0
+packageData.devDependencies['@types/react-router'] = void 0
 
 fs.writeFileSync(packageFile, JSON.stringify(packageData, null, 2))
 
