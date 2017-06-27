3ee89643d0ca780af80afa29e8e375a54f68c718
myxvisual <myxvisual@live.com>
2017-06-27 17:55:40 +0800

    fix: Fixed missing index not export Toast component

--- src/index.ts
+++ src/index.ts
@@ -2,23 +2,30 @@ export { default as AppBarButton, AppBarButtonButtonProps } from "./AppBarButton
 export { default as AppBarSeparator, AppBarSeparatorProps } from "./AppBarSeparator";
 export { default as ArticleCard, ArticleCardProps, ArticleCardState } from "./ArticleCard";
 export { default as AutoSuggestBox, AutoSuggestBoxProps, AutoSuggestBoxState } from "./AutoSuggestBox";
+
 export { default as Button, ButtonProps, ButtonState } from "./Button";
+
 export { default as CalendarDatePicker, CalendarDatePickerProps, CalendarDatePickerState } from "./CalendarDatePicker";
 export { default as CalendarView, CalendarViewProps, CalendarViewState } from "./CalendarView";
 export { default as CheckBox, CheckBoxProps, CheckBoxState } from "./CheckBox";
 export { default as ColorPicker, ColorPickerProps, ColorPickerState } from "./ColorPicker";
 export { default as CommandBar, CommandBarProps, CommandBarState } from "./CommandBar";
 export { default as ContentDialog, ContentDialogProps } from "./ContentDialog";
+
 export { default as DatePicker, DatePickerProps, DatePickerState } from "./DatePicker";
 export { default as Dialog, DialogProps, DialogState } from "./Dialog";
 export { default as DropDownMenu, DropDownMenuProps, DropDownMenuState } from "./DropDownMenu";
+
 export { default as FlipView, FlipViewProps, FlipViewState } from "./FlipView";
 export { default as FloatNav, FloatNavProps, FloatNavState } from "./FloatNav";
 export { default as Flyout, FlyoutProps } from "./Flyout";
 export { default as FlyoutContent, FlyoutContentProps, FlyoutContentState } from "./FlyoutContent";
+
 export { default as HyperLink, HyperLinkProps } from "./HyperLink";
+
 export { default as Icon, IconProps, IconState } from "./Icon";
 export { default as IconButton, IconButtonProps } from "./IconButton";
+
 export { default as Image, ImageProps } from "./Image";
 export { default as Link, LinkProps, LinkState } from "./Link";
 export { default as ListView, ListViewProps, ListViewState } from "./ListView";
@@ -26,12 +33,15 @@ export { default as MarkdownRender, MarkdownRenderProps } from "./MarkdownRender
 export { default as MediaPlayer, MediaPlayerProps, MediaPlayerState } from "./MediaPlayer";
 export { default as NavigationView, NavigationViewProps, NavigationViewState } from "./NavigationView";
 export { default as Menu, MenuProps, MenuState, MenuItem, MenuItemProps } from "./Menu";
+
 export { default as PasswordBox, PasswordBoxProps, PasswordBoxState } from "./PasswordBox";
 export { default as ProgressBar, ProgressBarProps, ProgressBarState } from "./ProgressBar";
 export { default as ProgressRing, ProgressRingProps } from "./ProgressRing";
+
 export { default as RadioButton, RadioButtonProps, RadioButtonState } from "./RadioButton";
 export { default as RatingControl, RatingControlProps, RatingControlState } from "./RatingControl";
 export { default as RenderToBody, RenderToBodyProps } from "./RenderToBody";
+
 export { default as ScrollBar, ScrollBarProps, ScrollBarState } from "./ScrollBar";
 export { default as ScrollReveal, ScrollRevealProps } from "./ScrollReveal";
 export { default as SemanticZoom, SemanticZoomProps, SemanticZoomState } from "./SemanticZoom";
@@ -40,10 +50,12 @@ export { default as Slider, SliderProps, SliderState } from "./Slider";
 export { default as SplitView, SplitViewPaneProps, SplitViewPane } from "./SplitView";
 export { default as SplitViewCommand, SplitViewCommandProps } from "./SplitViewCommand";
 export { default as Swipe, SwipeProps, SwipeState } from "./Swipe";
+
 export { default as Tabs, TabsProps, TabsState } from "./Tabs";
 export { default as TextBox, TextBoxProps, TextBoxState } from "./TextBox";
 export { default as Theme, ThemeProps, ThemeState } from "./Theme";
 export { default as TimePicker, TimePickerProps, TimePickerState } from "./TimePicker";
+export { default as Toast, ToastProps, ToastState } from "./Toast";
 export { default as Toggle, ToggleProps, ToggleState } from "./Toggle";
 export { default as Tooltip, TooltipProps, TooltipState } from "./Tooltip";
 export { default as TreeView, TreeViewProps, TreeViewState } from "./TreeView";
