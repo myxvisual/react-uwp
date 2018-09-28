e120cbbf82b23d8b0cd468eb719202eff33de3d5
myxvisual <myxvisual@live.com>
2018-09-28 09:05:53 +0800

    fix: Fixed navigator.userAgent empty error and Close #60

--- src/common/electron/IS_ELECTRON_ENV.ts
+++ src/common/electron/IS_ELECTRON_ENV.ts
@@ -1,4 +1,6 @@
-const userAgent = typeof navigator === "undefined" ? "" : navigator.userAgent.toLowerCase();
+const userAgent = (
+  typeof navigator !== "undefined" && navigator.userAgent && navigator.userAgent.toLowerCase()
+) || "";
 const isElectronEnv = userAgent.includes(" electron/");
 
 export default isElectronEnv;
