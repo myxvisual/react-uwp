6b8cb6243e9c7c968dabace02ce1441bdc781321
myxvisual <myxvisual@live.com>
2019-01-23 14:14:57 +0800

    docs: Update scripts

--- scripts/nextGitTag.js
+++ scripts/nextGitTag.js
@@ -1,5 +1,7 @@
 const { execSync } = require('child_process')
 const runCommandsAndLog = require('./runCommandsAndLog')
+const fs = require('fs')
+process.chdir(__dirname)
 
 let version = execSync('git describe --abbrev=0 --tags').toString().trim() || 'v1.0.0'
 console.log(`current git tag version is ${version}`)
@@ -10,6 +12,10 @@ const nextVersionNum = versionNum + 1
 const nextVersionStr = `${nextVersionNum}`
 const nextVersion = `v${nextVersionStr.slice(0, nextVersionStr.length - 2)}${nextVersionStr.slice(-2).replace(/(\w)/g, $1 => `.${$1}`)}`
 console.log(`next git tag version is ${nextVersion}`)
+
+const packageData = require('../package.json')
+packageData.version = `${nextVersion}`
+fs.writeFileSync('../package.json', JSON.stringify(packageData, null, 2))
 
 runCommandsAndLog(
   `git tag ${nextVersion}`,
