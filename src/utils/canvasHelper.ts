e6ba24d0624abd7eaf3cc1452dfc40697c169612
myxvisual <myxvisual@live.com>
2019-12-05 18:57:22 +0800

    feat: Update Canvas helpers

--- src/utils/canvasHelper.ts
+++ src/utils/canvasHelper.ts
@@ -35,3 +35,58 @@ export function isSupportWebGL() {
     return isSupportedWebGL;
   }
 }
+
+export interface Point {
+  x: number;
+  y: number;
+}
+
+export function deg2rad(deg: number) {
+  return Math.PI / 180 * deg;
+}
+
+export function rad2deg(rad: number) {
+  return rad / Math.PI * 180;
+}
+
+export function getRadEndPos(pos: Point, radius: number, rad: number) {
+  return { x: pos.x + radius * Math.cos(rad), y: pos.y + radius * Math.sin(rad) };
+}
+
+export function getRegularPolygon(center= { x: 0, y: 0 }, radius = 1, angleSize = 4) {
+  const isDoublePolygon = angleSize % 2 === 0;
+  const singleRad = (angleSize - 2) * Math.PI / angleSize;
+  const singleAngle = 360 / angleSize;
+  const rads: number[] = [];
+  const angles: number[] = [];
+  const points: Point[] = [];
+  const offsetRad = isDoublePolygon ? (Math.PI / 2 - singleRad / 2) : Math.PI / 2;
+  // Set polygon position is vertical.
+  const offsetAngle = isDoublePolygon ? (90 - singleAngle / 2) : 90;
+
+  for (let ind = 0; ind < angleSize; ind++) {
+    const rad = (ind * singleRad + offsetRad) % (Math.PI * 2);
+    const angle = (ind * singleAngle + offsetAngle) % 360;
+    const point = getRadEndPos(center, radius, rad);
+    rads.push(rad);
+    angles.push(angle);
+    points.push(point);
+  }
+
+  return {
+    rads,
+    angles,
+    points
+  };
+}
+
+export function isPatternRegularPolygon(sides: number) {
+  const insideAngle = 360 / sides;
+  const outSideAngle = 180 - insideAngle;
+
+  // result is [1, 3, 4, 6].
+  return 360 % outSideAngle === 0;
+}
+
+// const res = getRegularPolygon({ x: 0, y: 0 }, Math.sqrt(2));
+// console.log(res);
